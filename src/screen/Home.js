330f64e00654c5dc6910786d2ac1074dd03a77e9
Fatema-binbytes <fatema.binbytes@gmail.com>
2019-12-18 16:20:55 +0530

    OneSignal added

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -4,6 +4,7 @@ import {View, Image, StatusBar} from 'react-native';
 import {SlideAnimation} from 'react-native-popup-dialog';
 import {connect} from 'react-redux';
 import codePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import {user} from '../store/action';
 import colors from '../config/colors';
@@ -21,9 +22,33 @@ class Home extends Component {
     this.state = {
       show: false,
     };
+    OneSignal.init('4b11b264-0835-4cfe-a015-735452972dfa');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
   }
   componentDidMount() {
     this.checkUpdate();
+  }
+  onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
   }
   checkUpdate() {
     codePush.sync();
